662282efbcff41be0e4cd4364a4049c9d2dff24e
Cormanz <mrccay12@gmail.com>
2023-02-26 10:07:19 -0500

    Generalize Knight Movement to Board Sizes

--- src/board/pieces/king.rs
+++ src/board/pieces/king.rs
@@ -12,22 +12,22 @@ fn right_one(from: BitBoard, edges: &Edges) -> BitBoard {
     (from >> 1) & &(!(edges.left))
 }
 
-fn up_one(from: BitBoard, rows: Rows, edges: &Edges) -> BitBoard {
-    from >> rows & &(!(edges.bottom))
+fn up_one(from: BitBoard, cols: Cols, edges: &Edges) -> BitBoard {
+    from >> cols & &(!(edges.bottom))
 }
 
-fn down_one(from: BitBoard, rows: Rows, edges: &Edges) -> BitBoard {
-    from << rows & &(!(edges.top))
+fn down_one(from: BitBoard, cols: Cols, edges: &Edges) -> BitBoard {
+    from << cols & &(!(edges.top))
 }
 
 impl Piece for KingPiece {
     fn generate_moves(&self, board: &Board, mut from: BitBoard) -> BitBoard {
-        let rows = board.state.rows;
+        let cols = board.state.cols;
         let edges = &board.state.edges[0];
         let mut moves = left_one(from, edges) | &right_one(from, edges);
         from |= &moves;
-        moves |= &up_one(from, rows, edges);
-        moves |= &down_one(from, rows, edges);
+        moves |= &up_one(from, cols, edges);
+        moves |= &down_one(from, cols, edges);
         moves
     }   
 
