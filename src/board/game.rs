3db63c513551a712f506e40247af863d93c33e96
Corman <32941017+Cormanz@users.noreply.github.com>
2023-03-07 03:11:11 +0000

    Lay the groundwork for FenArguments

--- src/board/game.rs
+++ src/board/game.rs
@@ -5,6 +5,14 @@ pub trait MoveRestrictions {
     fn duplicate(&self) -> Box<dyn MoveRestrictions>;
 }
 
+pub trait FenArgument {
+    /// `encode` takes in a board, and outputs what this FEN argument's encoded result would be (eg. for a team argument, it could be `"b"`)
+    fn encode(board: &Board) -> String;
+
+    /// `decode` takes in a board and an existing argument, and will modify the board to meet the argument (eg. changing the team to reflect the given arg team of `w`)
+    fn decode(board: &mut Board, arg: String);
+}
+
 pub enum FenTeams {
     Number,
     TeamNames(Vec<char>)
