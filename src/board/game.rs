2ad455134fdb3c4ce291e394c18dc12a8e7de53b
Corman <32941017+Cormanz@users.noreply.github.com>
2023-03-07 03:39:09 +0000

    Make FenArgument object-safe

--- src/board/game.rs
+++ src/board/game.rs
@@ -5,16 +5,30 @@ pub trait MoveRestrictions {
     fn duplicate(&self) -> Box<dyn MoveRestrictions>;
 }
 
+pub struct NumberedTeam;
+impl FenArgument for NumberedTeam {
+    fn encode(&self, board: &Board) -> String {
+        format!("{}", board.state.moving_team)
+    }
+
+    fn decode(&self, board: &mut Board, arg: &str) -> Result<(), FenDecodeError> {
+        board.state.moving_team = arg.parse::<u32>()
+            .map_err(|_| FenDecodeError::InvalidArgument(format!("{} is not a valid numerical team value", arg)))?;
+        
+        Ok(())
+    }
+}
+
 pub enum FenDecodeError {
     InvalidArgument(String)
 }
 
 pub trait FenArgument {
     /// `encode` takes in a board, and outputs what this FEN argument's encoded result would be (eg. for a team argument, it could be `"b"`)
-    fn encode(board: &Board) -> String;
+    fn encode(&self, board: &Board) -> String;
 
     /// `decode` takes in a board and an existing argument, and will modify the board to meet the argument (eg. changing the team to reflect the given arg team of `w`)
-    fn decode(board: &mut Board, arg: &str) -> Result<(), FenDecodeError>;
+    fn decode(&self, board: &mut Board, arg: &str) -> Result<(), FenDecodeError>;
 }
 
 pub enum FenTeams {
