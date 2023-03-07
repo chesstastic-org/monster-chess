18ef59b6e3d7884b42a2121c581bffbbd07e6487
Corman <32941017+Cormanz@users.noreply.github.com>
2023-03-07 03:33:28 +0000

    Final FenArgument Draft Fix

--- src/board/game.rs
+++ src/board/game.rs
@@ -5,12 +5,16 @@ pub trait MoveRestrictions {
     fn duplicate(&self) -> Box<dyn MoveRestrictions>;
 }
 
+pub enum FenDecodeError {
+    InvalidArgument(String)
+}
+
 pub trait FenArgument {
     /// `encode` takes in a board, and outputs what this FEN argument's encoded result would be (eg. for a team argument, it could be `"b"`)
-    fn encode(board: &Board) -> &str;
+    fn encode(board: &Board) -> String;
 
     /// `decode` takes in a board and an existing argument, and will modify the board to meet the argument (eg. changing the team to reflect the given arg team of `w`)
-    fn decode(board: &mut Board, arg: &str);
+    fn decode(board: &mut Board, arg: &str) -> Result<(), FenDecodeError>;
 }
 
 pub enum FenTeams {
