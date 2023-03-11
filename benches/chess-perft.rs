f24a11c68a5e102e7fbb138d7f08be2118fff925
Corman <32941017+Cormanz@users.noreply.github.com>
2023-03-11 16:47:47 +0000

    Add Benchmark for Perft

--- benches/chess-perft.rs
+++ benches/chess-perft.rs
@@ -1,1 +1,20 @@
-use monster_chess::{games::chess::game::Chess};
+use monster_chess::{games::chess::Chess, board::Board};
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+fn startpos(depth: u32) {
+    let mut board = Board::new(
+        Chess::create(),
+        2,
+        (8, 8),
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+    );
+
+    board.sub_perft(depth);
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("startpos", |b| b.iter(|| startpos(4)));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
