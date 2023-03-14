89cbc11d5d24be17c26d64c5046e5721b5cf02f2
Corman <32941017+Cormanz@users.noreply.github.com>
2023-03-14 20:22:28 +0000

    Implement Ataxx Passing Moves

--- src/games/ataxx/controller.rs
+++ src/games/ataxx/controller.rs
@@ -3,6 +3,19 @@ use crate::{board::{game::MoveController, Board, actions::Action}, bitboard::Bit
 pub struct AtaxxMoveController;
 impl<const T: usize> MoveController<T> for AtaxxMoveController {
     fn transform_moves(&self, board: &mut Board<T>, mode: u32, actions: Vec<Option<Action>>) -> Vec<Option<Action>> {
-        actions
+        // No Legal Moves
+        if actions.len() == 0 {
+            let board_mask = BitBoard::starting_at_lsb(0, 49);
+            let filled_squares = board.state.pieces[0];
+            let empty_squares = board_mask & !filled_squares;
+
+            if empty_squares.count_ones() > 0 && filled_squares.count_ones() > 0 {
+                vec![ None ]
+            } else {
+                actions
+            }
+        } else {
+            actions
+        }
     }
 }
