d9ddbb8208215171f7b1f7260ac4c55b2e56fab1
Corman <32941017+Cormanz@users.noreply.github.com>
2023-03-29 20:25:18 +0000

    Fix Zobrist Hash

--- src/board/zobrist.rs
+++ src/board/zobrist.rs
@@ -11,34 +11,41 @@ pub struct ZobristHashTable<const T: usize> {
 }
 
 impl<const T: usize> ZobristHashTable<T> {
-    fn get_gap_index(&self, position: u16) -> usize {
-        position as usize
+    fn get_moving_team_index(&self, moving_team: u16) -> usize {
+        moving_team as usize
     }
 
-    fn get_first_move_index(&self, position: u16) -> usize {
-        (position + (self.squares)) as usize
+    fn get_gap_index(&self, moving_team: u16, position: u16) -> usize {
+        (moving_team as usize) + position as usize
     }
 
-    fn get_piece_index(&self, position: u16, piece_type: u16, team: u16) -> usize {
-        (position + (self.squares * (1 + (piece_type + (self.pieces * team))))) as usize
+    fn get_first_move_index(&self, moving_team: u16, position: u16) -> usize {
+        (moving_team as usize) + (position + (self.squares)) as usize
+    }
+
+    fn get_piece_index(&self, moving_team: u16, position: u16, piece_type: u16, team: u16) -> usize {
+        (moving_team as usize) + (position + (self.squares * (1 + (piece_type + (self.pieces * team))))) as usize
     }
 
     pub fn compute(&self, board: &Board<T>) -> u64 {
         let mut hash = 0;
+        let moving_team = board.state.moving_team;
+
+        hash ^= self.table[self.get_moving_team_index(moving_team)];
 
         for gap_pos in board.state.gaps.iter_set_bits(board.state.squares) {
-            hash ^= self.table[self.get_gap_index(gap_pos)];
+            hash ^= self.table[self.get_gap_index(moving_team, gap_pos)];
         }
 
         for first_move_pos in board.state.first_move.iter_set_bits(board.state.squares) {
-            hash ^= self.table[self.get_first_move_index(first_move_pos)];
+            hash ^= self.table[self.get_first_move_index(moving_team, first_move_pos)];
         }
 
         for piece_type in 0..self.pieces {
             for team in 0..self.teams {
                 let piece_team_board = board.state.pieces[piece_type as usize] & board.state.teams[team as usize];
                 for piece_pos in piece_team_board.iter_set_bits(board.state.squares) {
-                    hash ^= self.table[self.get_piece_index(piece_pos, piece_type, team)];
+                    hash ^= self.table[self.get_piece_index(moving_team, piece_pos, piece_type, team)];
                 }
             }
         }       
@@ -47,10 +54,10 @@ impl<const T: usize> ZobristHashTable<T> {
     }
 
     pub fn generate(squares: u16, teams: u16, pieces: u16, extra_hashes: usize, get_random: impl Fn() -> u64) -> ZobristHashTable<T> {
-        let hashes = (squares * (2 + (pieces * teams))) as usize;
         let base_len = (
-            (squares - 1) + (squares * (1 + ((pieces - 1) + (pieces * (teams - 1))))) + 1
+            (teams - 1) + (squares) + (squares * (1 + ((pieces - 1) + (pieces * (teams - 1))))) + 1
         ) as usize;
+        let hashes = base_len + (extra_hashes as usize);
         let mut zobrist = ZobristHashTable {
             table: vec![ 0; hashes ],
             squares: squares,
